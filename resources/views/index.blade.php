d97c31fc4f7bde95b9e5f0dd543e180834d101c3
Giovani Tzec <giovanni.tzec@gmail.com>
2023-02-07 11:53:09 -0600

    asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,19 +11,19 @@
     <title>Mundo Céramico</title>
 
     <!-- Scripts -->
-    <script src="{{ mix('js/manifest.js') }}" defer></script>
-    <script src="{{ mix('js/vendor.js') }}" defer></script>
-    <script src="{{ mix('js/app.js') }}" defer></script>
-    <script src="{{ mix('js/btnScrollToTop.js') }}" defer></script>
-    <script src="{{ mix('js/scrollreveal.min.js') }}" defer></script>
-    <script src="{{ mix('js/main.js') }}" defer></script>
+    <script src="{{ asset('js/manifest.js') }}" defer></script>
+    <script src="{{ asset('js/vendor.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/btnScrollToTop.js') }}" defer></script>
+    <script src="{{ asset('js/scrollreveal.min.js') }}" defer></script>
+    <script src="{{ asset('js/main.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ mix('css/app.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/app.css')}}" rel="stylesheet">
 
     <!-- Favicon -->
     <link rel="icon" href="{{ ('img/layout-wtf.svg') }}">
